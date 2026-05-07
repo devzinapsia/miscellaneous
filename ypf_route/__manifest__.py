4f904cf4aa5c2759021e1790651fc13ba0924dbf
devzinapsia <dev@zinapsia.com>
2026-05-07 16:12:22 -0300

    feat: add YPF button to invoice list view

--- ypf_route/__manifest__.py
+++ ypf_route/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "YPF Ruta - Importación de Facturas",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "category": "Accounting",
     "summary": "Importación automática de planillas YPF Ruta",
     "author": "Zinapsia",
@@ -15,11 +15,14 @@
         "wizard/ypf_import_wizard_view.xml",
         "wizard/ypf_menu.xml",
     ],
-    "external_dependencies": {
-        "python": [
-            "pandas",
-            "openpyxl",
+    "assets": {
+        "web.assets_backend": [
+            "ypf_route/static/src/js/ypf_list_button.js",
+            "ypf_route/static/src/xml/ypf_list_button.xml",
         ],
+    },
+    "external_dependencies": {
+        "python": ["pandas", "openpyxl"],
     },
     "installable": True,
     "license": "LGPL-3",
